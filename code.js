273816ec5de70c0dd2bd3d87b356c7e0f0fa3ab4
22dcolli <144738226+22dcolli@users.noreply.github.com>
2024-02-12 17:39:59 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,33 @@
-function binarySearch(list, element) {
+function binarySearch(list, element) 
+{
+  //console.log(list);
+
+  let first = 0;
+  let last = list.length - 1;
+  
+
+  while (first <= last) 
+  {
+    let middle = Math.floor((first + last) / 2);//this is the key line that fixed my initial code, for future me, multiple sources say this is the simplest fix.
+    //console.log(middle);
+    if (list[middle] == element)
+    {
+      //console.log("found element at index:", middle);
+      return middle;
+    } 
+    else 
+    {
+      if (element < list[middle]) 
+      {
+        //console.log("its to the left");
+        last = middle - 1;
+      } 
+      else 
+      {
+        //console.log("its to the right");
+        first = middle + 1;
+      }
+    }
+  }
     return -1;
 }
